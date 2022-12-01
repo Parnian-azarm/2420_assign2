deaa112cb24b490fac39a6754ca60b10cfac09bc
Summry <nazira.fakhrurradi7@gmail.com>
2022-11-30 17:10:10 -0800

    Changed the fastify route to /api. Changed the port number to 5050

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 const fastify = require('fastify')({ logger: true })
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
+fastify.get('/api', async (request, reply) => {
   return { hello: 'Server x' }
 })
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 5050 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
